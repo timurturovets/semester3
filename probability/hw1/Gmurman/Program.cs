c8eafdd7c845d4c8839c1762d97c13012dcb12c1
teletraan <timurturovets@mail.ru>
2026-02-19 03:30:41 +0300

    probability hw1 task13

--- probability/hw1/Gmurman/Program.cs
+++ probability/hw1/Gmurman/Program.cs
@@ -20,6 +20,7 @@ public class Gmurman
             { 27, Task27.Run },
             { 28, Task28.Run },
             { 29, Task29.Run },
+            { 30, Task30.Run },
         };
         
         Console.WriteLine("Задачи из пособия Гмурмана");
@@ -35,6 +36,7 @@ public class Gmurman
         Console.WriteLine("27. На отрезок OA длины L...");
         Console.WriteLine("28. В круг радиуса R помещён...");
         Console.WriteLine("29. Плоскость разграфлена параллельными прямыми...");
+        Console.WriteLine("30. На плоскость с нанесённой сеткой квадратов...");
         Console.WriteLine("0. Выход");
         Console.WriteLine();
         
---
+++ probability/hw1/Gmurman/Task30.cs
@@ -0,0 +1,24 @@
+﻿namespace Gmurman;
+
+internal class Task30
+{
+    internal static void Run()
+    {
+        const double a = 1.0;
+        const double r = 0.4;
+        const int experiments = 1_000_000;
+
+        var hits = 0;
+        var rnd = new Random();
+
+        for (var i = 0; i < experiments; i++)
+        {
+            var x = rnd.NextDouble() * a;
+            var y = rnd.NextDouble() * a;
+
+            if (x >= r && x <= a - r && y >= r && y <= a - r) hits++;
+        }
+        
+        Console.WriteLine($"Вычисленная вероятность: {(double) hits / experiments}");
+    }
+}
