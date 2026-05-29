d3405d7975940d491d1d3d85ccb3c4ad8f9d94cb
Timur <89309050+timurturovets@users.noreply.github.com>
2026-05-29 03:13:09 +0300

    arithmetic hw7

---
+++ arithmetic/hw7/application/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Application;
+
+public class Program
+{
+    private delegate void Runner(string[] args);
+    internal static void Main(string[] args)
+    {
+        var runners = new List<Runner>()
+        {
+            
+        };
+        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Эта программа решает задачи с применением двоичных многочленов.");
+
+            Console.WriteLine("0. Выход");
+            Console.WriteLine();
+            
+            int choice;
+            do Console.Write("Выберите задачу: ");
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > runners.Count);
+
+            if (choice == 0) return;
+
+            Console.WriteLine();
+            
+            runners[choice - 1](args);
+            
+            Console.WriteLine();
+            Console.Write("Нажмите любую кнопку, чтобы продолжить...");
+            Console.ReadKey();
+        }
+    }
+}
